0a0dc1c2afd301776af7cbb4e733120f19538142
Clevis74 <clevis.pietro@gmail.com>
2025-08-10 15:04:18 -0300

    Ajusta vite.config.ts e atualiza yarn.lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      "617d6955-e336-447e-9336-782340268964.preview.emergentagent.com",
-      "14cd1f93-550c-4800-ac60-39195504d5ec.preview.emergentagent.com"
-    ]
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    host: true
+  }
 });
